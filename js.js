fc7b59eae8c960e0ca3a0dd49e78327ae137a339
Davi de Almeida Nascimento <118446616+dexterpol@users.noreply.github.com>
2023-08-14 10:19:11 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function abrirModal() {
+    const modal = document.getElementById('modal');
+    const videoImage = document.getElementById('video-image');
+    
+    
+    modal.style.display = 'block';
+}
+
+function fecharModal() {
+    const modal = document.getElementById('modal');
+    const videoImage = document.getElementById('video-image');
+
+    
+    modal.style.display = 'none';
+}
